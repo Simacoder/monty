7b0f60f1c52644ecacfe4986d464125965a19b98
Emmanuel <emmanuelozigue@gmail.com>
2022-11-13 06:59:03 -0800

    all done

--- swap.c
+++ swap.c
@@ -13,11 +13,10 @@
  * Return: void
  */
 
-void swap(stack_t **stack, unsigned int line_cnt) 
-{  
+void swap(stack_t **stack, unsigned int line_cnt)
+{
 stack_t *tmp = NULL;
 int tmp_n = 0;
-  
 if (!stack || !*stack || !((*stack)->next))
 {
 fprintf(stderr, "L%d: can't swap, stack too short\n", line_cnt);
@@ -27,5 +26,5 @@ tmp = *stack;
 tmp_n = tmp->n;
 tmp->n = tmp_n;
 tmp->n = tmp->next->n;
-tmp->next->n = tmp_n; 
+tmp->next->n = tmp_n;
 }
